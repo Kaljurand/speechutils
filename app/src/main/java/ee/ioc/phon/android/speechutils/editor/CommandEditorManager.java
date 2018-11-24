ea3275b39cf174e6706a41dababf7b608c87670e
Kaarel Kaljurand <kaljurand@gmail.com>
2018-11-25 00:17:41 +0100

    Use Java 1.8 lambdas for the editor commands

--- app/src/main/java/ee/ioc/phon/android/speechutils/editor/CommandEditorManager.java
+++ app/src/main/java/ee/ioc/phon/android/speechutils/editor/CommandEditorManager.java
@@ -58,411 +58,208 @@ public class CommandEditorManager {
 
         Map<String, EditorCommand> aMap = new HashMap<>();
 
-        aMap.put(KEY_UP, new EditorCommand() {
+        aMap.put(KEY_UP, (ce, args) -> ce.keyUp());
 
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.keyUp();
-            }
-        });
-        aMap.put(KEY_DOWN, new EditorCommand() {
+        aMap.put(KEY_DOWN, (ce, args) -> ce.keyDown());
 
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.keyDown();
-            }
-        });
-        aMap.put(KEY_LEFT, new EditorCommand() {
+        aMap.put(KEY_LEFT, (ce, args) -> ce.keyLeft());
 
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.keyLeft();
-            }
-        });
-        aMap.put(KEY_RIGHT, new EditorCommand() {
+        aMap.put(KEY_RIGHT, (ce, args) -> ce.keyRight());
 
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.keyRight();
-            }
-        });
-
-        aMap.put(UNDO, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                int steps = 1;
-                if (args != null && args.length > 0) {
-                    try {
-                        steps = Integer.parseInt(args[0]);
-                    } catch (NumberFormatException e) {
-                        // Intentional
-                    }
+        aMap.put(UNDO, (ce, args) -> {
+            int steps = 1;
+            if (args != null && args.length > 0) {
+                try {
+                    steps = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    // Intentional
                 }
-                return ce.undo(steps);
             }
+            return ce.undo(steps);
         });
 
-        aMap.put(COMBINE, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                int numOfOps = 2;
-                if (args != null && args.length > 0) {
-                    try {
-                        numOfOps = Integer.parseInt(args[0]);
-                    } catch (NumberFormatException e) {
-                        // Intentional
-                    }
+        aMap.put(COMBINE, (ce, args) -> {
+            int numOfOps = 2;
+            if (args != null && args.length > 0) {
+                try {
+                    numOfOps = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    // Intentional
                 }
-                return ce.combine(numOfOps);
             }
+            return ce.combine(numOfOps);
         });
 
-        aMap.put(APPLY, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                int steps = 1;
-                if (args != null && args.length > 0) {
-                    try {
-                        steps = Integer.parseInt(args[0]);
-                    } catch (NumberFormatException e) {
-                        // Intentional
-                    }
+        aMap.put(APPLY, (ce, args) -> {
+            int steps = 1;
+            if (args != null && args.length > 0) {
+                try {
+                    steps = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    // Intentional
                 }
-                return ce.apply(steps);
             }
+            return ce.apply(steps);
         });
 
-        aMap.put(MOVE_ABS, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                int pos = 0;
-                if (args != null && args.length > 0) {
-                    try {
-                        pos = Integer.parseInt(args[0]);
-                    } catch (NumberFormatException e) {
-                        // Intentional
-                    }
+        aMap.put(MOVE_ABS, (ce, args) -> {
+            int pos = 0;
+            if (args != null && args.length > 0) {
+                try {
+                    pos = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    // Intentional
                 }
-                return ce.moveAbs(pos);
             }
+            return ce.moveAbs(pos);
         });
 
-        aMap.put(MOVE_REL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                int pos = 1;
-                if (args != null && args.length > 0) {
-                    try {
-                        pos = Integer.parseInt(args[0]);
-                    } catch (NumberFormatException e) {
-                        // Intentional
-                    }
+        aMap.put(MOVE_REL, (ce, args) -> {
+            int pos = 1;
+            if (args != null && args.length > 0) {
+                try {
+                    pos = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    // Intentional
                 }
-                return ce.moveRel(pos);
             }
+            return ce.moveRel(pos);
         });
 
-        aMap.put(KEY_CODE, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args != null && args.length > 0) {
-                    try {
-                        return ce.keyCode(Integer.parseInt(args[0]));
-                    } catch (NumberFormatException e) {
-                        // Intentional
-                    }
+        aMap.put(KEY_CODE, (ce, args) -> {
+            if (args != null && args.length > 0) {
+                try {
+                    return ce.keyCode(Integer.parseInt(args[0]));
+                } catch (NumberFormatException e) {
+                    // Intentional
                 }
+            }
+            return null;
+        });
+
+        aMap.put(KEY_CODE_STR, (ce, args) -> {
+            if (args == null || args.length < 1) {
                 return null;
             }
+            return ce.keyCodeStr(args[0]);
         });
 
-        aMap.put(KEY_CODE_STR, new EditorCommand() {
+        aMap.put(SELECT, (ce, args) -> {
+            if (args == null || args.length < 1) {
+                return null;
+            }
+            return ce.select(args[0]);
+        });
 
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
+        aMap.put(SELECT_RE_BEFORE, (ce, args) -> {
+            if (args == null || args.length < 1) {
+                return null;
+            }
+            return ce.selectReBefore(args[0]);
+        });
+
+        aMap.put(SELECT_RE_AFTER, (ce, args) -> {
+            if (args == null || args.length < 1) {
+                return null;
+            }
+            int n = 1;
+            if (args.length > 1) {
+                try {
+                    n = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    // Intentional
                 }
-                return ce.keyCodeStr(args[0]);
             }
+            return ce.selectReAfter(args[0], n);
         });
 
-        aMap.put(SELECT, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
-                }
-                return ce.select(args[0]);
+        aMap.put(REPLACE_SEL_RE, (ce, args) -> {
+            if (args == null || args.length < 2) {
+                return null;
             }
+            return ce.replaceSelRe(args[0], args[1]);
         });
 
-        aMap.put(SELECT_RE_BEFORE, new EditorCommand() {
+        aMap.put(DELETE_LEFT_WORD, (ce, args) -> ce.deleteLeftWord());
 
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
-                }
-                return ce.selectReBefore(args[0]);
+        aMap.put(REPLACE, (ce, args) -> {
+            if (args == null || args.length < 1) {
+                return null;
             }
+            String text1 = args[0];
+            String text2 = args.length > 1 ? args[1] : "";
+            return ce.replace(text1, text2);
         });
 
-        aMap.put(SELECT_RE_AFTER, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
-                }
-                int n = 1;
-                if (args.length > 1) {
-                    try {
-                        n = Integer.parseInt(args[1]);
-                    } catch (NumberFormatException e) {
-                        // Intentional
-                    }
-                }
-                return ce.selectReAfter(args[0], n);
+        aMap.put(REPLACE_SEL, (ce, args) -> {
+            if (args == null || args.length < 1) {
+                return null;
             }
+            return ce.replaceSel(args[0]);
         });
 
-        aMap.put(REPLACE_SEL_RE, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 2) {
-                    return null;
-                }
-                return ce.replaceSelRe(args[0], args[1]);
+        aMap.put(SAVE_CLIP, (ce, args) -> {
+            if (args == null || args.length < 2) {
+                return null;
             }
+            return ce.saveClip(args[0], args[1]);
         });
 
-        aMap.put(DELETE_LEFT_WORD, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.deleteLeftWord();
+        aMap.put(LOAD_CLIP, (ce, args) -> {
+            if (args == null || args.length < 1) {
+                return null;
             }
+            return ce.loadClip(args[0]);
         });
 
-        aMap.put(REPLACE, new EditorCommand() {
+        aMap.put(SHOW_CLIPBOARD, (ce, args) -> ce.showClipboard());
 
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
-                }
-                String text1 = args[0];
-                String text2 = args.length > 1 ? args[1] : "";
-                return ce.replace(text1, text2);
+        aMap.put(CLEAR_CLIPBOARD, (ce, args) -> ce.clearClipboard());
+
+        aMap.put(UC_SEL, (ce, args) -> ce.ucSel());
+
+        aMap.put(LC_SEL, (ce, args) -> ce.lcSel());
+
+        aMap.put(INC_SEL, (ce, args) -> ce.incSel());
+
+        aMap.put(SELECT_ALL, (ce, args) -> ce.selectAll());
+
+        aMap.put(CUT, (ce, args) -> ce.cut());
+
+        aMap.put(CUT_ALL, (ce, args) -> ce.cutAll());
+
+        aMap.put(DELETE_ALL, (ce, args) -> ce.deleteAll());
+
+        aMap.put(COPY, (ce, args) -> ce.copy());
+
+        aMap.put(COPY_ALL, (ce, args) -> ce.copyAll());
+
+        aMap.put(PASTE, (ce, args) -> ce.paste());
+
+        aMap.put(IME_ACTION_PREVIOUS, (ce, args) -> ce.imeActionPrevious());
+
+        aMap.put(IME_ACTION_NEXT, (ce, args) -> ce.imeActionNext());
+
+        aMap.put(IME_ACTION_DONE, (ce, args) -> ce.imeActionDone());
+
+        aMap.put(IME_ACTION_GO, (ce, args) -> ce.imeActionGo());
+
+        aMap.put(IME_ACTION_SEARCH, (ce, args) -> ce.imeActionSearch());
+
+        aMap.put(IME_ACTION_SEND, (ce, args) -> ce.imeActionSend());
+
+        aMap.put(ACTIVITY, (ce, args) -> {
+            if (args == null || args.length < 1) {
+                return null;
             }
+            return ce.activity(args[0]);
         });
 
-        aMap.put(REPLACE_SEL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
-                }
-                return ce.replaceSel(args[0]);
+        aMap.put(GET_URL, (ce, args) -> {
+            if (args == null || args.length < 1) {
+                return null;
             }
-        });
-
-        aMap.put(SAVE_CLIP, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 2) {
-                    return null;
-                }
-                return ce.saveClip(args[0], args[1]);
-            }
-        });
-
-        aMap.put(LOAD_CLIP, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
-                }
-                return ce.loadClip(args[0]);
-            }
-        });
-
-        aMap.put(SHOW_CLIPBOARD, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.showClipboard();
-            }
-        });
-
-        aMap.put(CLEAR_CLIPBOARD, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.clearClipboard();
-            }
-        });
-
-        aMap.put(UC_SEL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.ucSel();
-            }
-        });
-
-        aMap.put(LC_SEL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.lcSel();
-            }
-        });
-
-        aMap.put(INC_SEL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.incSel();
-            }
-        });
-
-        aMap.put(SELECT_ALL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.selectAll();
-            }
-        });
-
-        aMap.put(CUT, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.cut();
-            }
-        });
-
-        aMap.put(CUT_ALL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.cutAll();
-            }
-        });
-
-        aMap.put(DELETE_ALL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.deleteAll();
-            }
-        });
-
-        aMap.put(COPY, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.copy();
-            }
-        });
-
-        aMap.put(COPY_ALL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.copyAll();
-            }
-        });
-
-        aMap.put(PASTE, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.paste();
-            }
-        });
-
-        aMap.put(IME_ACTION_PREVIOUS, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.imeActionPrevious();
-            }
-        });
-
-        aMap.put(IME_ACTION_NEXT, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.imeActionNext();
-            }
-        });
-
-        aMap.put(IME_ACTION_DONE, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.imeActionDone();
-            }
-        });
-
-        aMap.put(IME_ACTION_GO, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.imeActionGo();
-            }
-        });
-
-        aMap.put(IME_ACTION_SEARCH, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.imeActionSearch();
-            }
-        });
-
-        aMap.put(IME_ACTION_SEND, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                return ce.imeActionSend();
-            }
-        });
-
-        aMap.put(ACTIVITY, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
-                }
-                return ce.activity(args[0]);
-            }
-        });
-
-        aMap.put(GET_URL, new EditorCommand() {
-
-            @Override
-            public Op getOp(CommandEditor ce, String[] args) {
-                if (args == null || args.length < 1) {
-                    return null;
-                }
-                return ce.getUrl(args[0]);
-            }
+            return ce.getUrl(args[0]);
         });
 
         EDITOR_COMMANDS = Collections.unmodifiableMap(aMap);
